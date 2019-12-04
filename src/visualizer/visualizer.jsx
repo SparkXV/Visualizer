da1b5e325b52fc234304d40d395d8b69e20ff65a
SparkXV <deepanshuvijay123@gmail.com>
2019-12-05 02:30:14 +0530

    New Commit

--- src/visualizer/visualizer.jsx
+++ src/visualizer/visualizer.jsx
@@ -9,11 +9,31 @@ export default class visualizer extends Component{
         this.state={};
     }
 
+    componentDidMount(){
+        const nodes=[];
+        for(let row=0;row<15;row++)
+        {
+            const currentRow=[];
+            for(let col=0;col<50;col++)
+            {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes});
+    }
     render(){
+
+        const {nodes}=this.state;
+        console.log(nodes);
         return (
-            <div>
-              foo
-            <Node></Node>
+            <div className="grid">
+              {nodes.map((row,rowIdx) =>
+                {
+                    return <div> {row.map((node,nodeIdx)=><Node></Node>)}
+                    </div>
+                })}
+            
             </div>
         );
     }
